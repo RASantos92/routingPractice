68b28d6eb5f933e32293561788a92752fd1cc393
Robert Santos <rasbms81608@gmail.com>
2020-10-06 21:34:40 -0500

    finishing logic in routs

--- src/main/java/com/example/demo/DojoController.java
+++ src/main/java/com/example/demo/DojoController.java
@@ -8,18 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class DojoController {
 	@RequestMapping("/m/{var}")
 	public String dojo(@PathVariable("var") String var) {
-		return "The " + var + " is awesome!";
-
+		String output = "";
+		if (var.equals("burbank-dojo")) {
+			output += "Burbank Dojo is located in Southern California";
+		} else if (var.equals("san-jose")) {
+			output += "SJ dojo is the headquearters";
+		} else {
+			output += var + " is awsome";
+		}
+		return output;
 	}
 
-	@RequestMapping("/m/{var}-{var1}")
-	public String location(@PathVariable("var") String var, @PathVariable("var1") String var1) {
-		return var + " " + var1 + "is located in Southern California";
-
-	}
-
-	public String location1(@PathVariable("var") String var, @PathVariable("var1") String var1) {
-		return var + " " + var1 + "is located in Southern California";
-
-	}
 }
